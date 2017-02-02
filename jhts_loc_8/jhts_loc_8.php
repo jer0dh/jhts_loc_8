1460075aed8c2953d0e785ba05c718f26b0568ab
jer0dh <jerod@jhtechservices.com>
2017-02-01 21:37:50 -0600

    Adding ability to save to geo mashup database, beginning adding localized variables

--- jhts_loc_8/jhts_loc_8.php
+++ jhts_loc_8/jhts_loc_8.php
@@ -49,6 +49,9 @@ class Jhts_loc_8 {
 
         //Add Ajax endpoint
         add_action( 'wp_ajax_loc_8_geocode', array($this, 'ajax_loc_8_geocode') );
+
+		// Hook to 'save_post' to save location data
+		add_action( 'save_post', array($this, 'save_post'), 11, 2 );
 	}
 
     public function scripts() {
@@ -60,9 +63,32 @@ class Jhts_loc_8 {
     }
     // Load our javascripts in admin when edit.php is run
     public function admin_scripts( $hook ){
-/*        if ( $hook != 'edit.php' ) {
+	    global $post;
+
+       if ( 'post.php' !== $hook  ) {
             return;
-        }*/
+        }
+
+        $local_vars = [];
+	    $location = $this->get_location_data($post->ID);
+	    if($location) {
+		    $local_vars['result'] = array(
+		    	'lat'       => esc_textarea($location['lat']),
+			    'long'      => esc_textarea($location['lng']),
+			    'address'   => esc_textarea($location['address']),
+			    'city'      => esc_textarea($location['locality_name']),
+			    'state'     => esc_textarea($location['admin_code']),
+			    'zip'       => esc_textarea($location['postal_code']),
+			    'country'   => esc_textarea($location['country_code'])
+		    );
+		    $local_vars['ajax_url'] = admin_url('admin-ajax.php');
+		    $local_vars['action'] = 'loc_8_geocode';
+		    $local_vars['mapTileLayer']= 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}';
+            $local_vars['mapAttribution'] = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
+            $local_vars['mapAccessId']= 'loc_8_wp';
+            $local_vars['mapMaxZoom'] = 18;
+            $local_vars['mapAccessToken'] = 'pk.eyJ1IjoiamVyMGRoIiwiYSI6ImNpeGo3MGRjaTAwNGIyd280ODJ0dzA1bm4ifQ.tFc-Mw0uY6Zf5056W_R5qw';
+	    }
         wp_enqueue_script('vuejs', plugins_url( '/js/dist/vendor/vue-@2.1.10.min.js', __FILE__), array(),'1.0.0',true);
         wp_enqueue_script('leafletjs', plugins_url( '/js/dist/vendor/leaflet@1.0.3/leaflet.js', __FILE__), array(),'1.0.0',true);
         wp_enqueue_script('scripts', plugins_url( '/js/dist/scripts.js', __FILE__), array('jquery','vuejs','leafletjs'),'1.0.0',true);
@@ -70,7 +96,37 @@ class Jhts_loc_8 {
         wp_enqueue_style('loc8styles', plugins_url('/css/style.css', __FILE__));
     }
 
+	/**
+	 * Get lat and lng data from Geo Mashup and from loc_8 custom fields
+	 *
+	 * @param $post_id
+	 * @return mixed Array of values or false if no location
+	 */
+	protected function get_location_data($post_id) {
 
+		$location = GeoMashupDB::get_object_location( 'post', $post_id, ARRAY_A );
+
+
+		return $location;
+	}
+    /*    [object_id] => 488
+    [geo_date] => 2017-02-01 22:46:29
+    [label] => Voyageurs
+    [id] => 59
+    [lat] => 32.3336372
+    [lng] => -95.2930756
+    [address] => 1329 S Beckham Ave
+    [saved_name] =>
+    [geoname] =>
+    [postal_code] => 75701
+    [country_code] => US
+    [admin_code] => TX
+    [sub_admin_code] =>
+    [locality_name] => Tyler
+    [post_author] => 15
+     *
+     *
+     */
     public function loc_8_meta_box_setup(){
         add_action( 'add_meta_boxes', array($this, 'loc_8_add_meta_box'));
     }
@@ -165,6 +221,46 @@ class Jhts_loc_8 {
         wp_send_json_success( array ("url" => $url, "results" => $results, "response" => json_decode( $response['body'] )));
     }
     // returns { success: true, data: Array}
+
+
+	/**
+	 * Check to see if we need to save location data.
+	 */
+	public function save_post($post_id, $post) {
+		if ( 'revision' == $post->post_type ) {
+			return;
+		}
+		error_log('In loc 8 save post method');
+
+		//TODO Check nonces and capabilities
+
+		//TODO Check for loc8_geo_date in POST
+		$geo_date = date( 'Y-m-d H:i:s' );
+
+		$post_location = [];
+		$post_location['lat'] = sanitize_text_field($_POST['loc-8-lat']);
+		$post_location['lng'] = sanitize_text_field($_POST['loc-8-long']);
+		$post_location['address'] = sanitize_text_field($_POST['loc-8-address']);
+		$post_location['locality_name'] = sanitize_text_field($_POST['loc-8-city']);
+		$post_location['admin_code'] = sanitize_text_field($_POST['loc-8-state']);
+		$post_location['postal_code'] = sanitize_text_field($_POST['loc-8-zip']);
+		$post_location['country_code'] = sanitize_text_field($_POST['loc-8-country']);
+
+		//TODO save custom fields to post
+
+
+		$location_id = GeoMashupDB::set_object_location( 'post', $post_id, $post_location, true, $geo_date );
+		if ( is_wp_error( $location_id ) ) {
+			error_log("error saving: ");
+			error_log(print_r($location_id, true));
+
+			update_post_meta( $post_id, 'geo_mashup_save_location_error', $location_id->get_error_message() );
+		}
+
+		return true;
+	}
+
+
     /**
 	 * Activate Plugin
 	 */
